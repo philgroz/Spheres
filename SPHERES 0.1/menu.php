aec8dd792090f73a2b9b4ba5867d36604fb33e8d
Philipp Grozinger <pg.graphite@gmail.com>
2013-07-26 08:01:37 +0930

    Prototype item structure in menu.php
    Added a prototype item structure for development purposes in the
    menu.php

--- SPHERES 0.1/menu.php
+++ SPHERES 0.1/menu.php
@@ -8,6 +8,17 @@
 		<script src="global.js" type="text/javascript"></script><!-- use the global javascript -->
 	</head>
 	<body>
-		<!-- requires html framework for javascript to manipulate -->
+		<!-- requires html framework for javascript to manipulate or write into -->
+		<!-- PG 26/7 below is a prototype item structure, used article tag to display an item -->
+		<article>
+			<img src="" /><!-- feature image of the item -->
+			<div>
+				<h2><!-- name of the item --></h2>
+				<p><!-- description of the item --></p>
+				<div><!-- this is the price tag - needs further customisation --></div>
+				<button>+</button>
+			</div>
+		</article>
+		<!-- /PG 26/7 -->
 	</body>
 </html>
